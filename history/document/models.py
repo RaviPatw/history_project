94dd3035e909adc4466a055a2f67dfdc7e2a8c6f
Ravi Patwardhan <ravipatwardhan123@gmail.com>
2025-06-06 14:44:31 -0400

    gb

--- history/document/models.py
+++ history/document/models.py
@@ -14,5 +14,6 @@ class Document(models.Model):
     link = models.URLField(blank=True, null=True)
     image = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     unit = models.CharField(max_length=5, choices=UNIT_CHOICES)
+    is_bacon = models.BooleanField(default=False)
     def __str__(self):
         return self.title
--- history/pages/views.py
+++ history/pages/views.py
@@ -8,6 +8,7 @@ def unit_page(request, unit_id):
     context={
         'documents':documents,
         'unit_id':unit_id
+        
     }
     return render(request, 'unit.html',context)
 def themes(request):
